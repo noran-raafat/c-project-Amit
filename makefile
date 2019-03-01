190d3fba92225760cbb4a78b70a8590f0a44b400
Noran Raafat <noran.raafat@optomatica.com>
2019-03-01 03:29:02 +0200

    divided code into separate files. solved some issues.

---
+++ makefile
@@ -0,0 +1,3 @@
+
+EXE:soccer_main.c soccer_input.c soccer_search.c soccer_stats.c config.h soccer.h
+	gcc soccer_main.c soccer_input.c soccer_search.c soccer_stats.c config.h soccer.h -o soccer.exe
---
+++ soccer.h
@@ -0,0 +1,30 @@
+/*******  FUNCTIONS PROTOTYPES  *******/
+void addDeleteMenu();
+void displayMenu();
+void addPlayer();
+void displayAllPlayer();
+char addClub();
+void displayAllClub();
+void searchForPlayerMenu();
+void printPlayerID(char);
+int searchPlayerName();
+int searchPlayerRank();
+void searchPlayerDeffence();
+void searchForClubMenu();
+void printClubID(char);
+int searchClubName();
+int searchClubRank();
+void deletePlayer();
+void deleteClub();
+void displayStats();
+char calcStats();
+void chooseClub(char );
+void searchPlayerSpeed();
+void searchPlayerAssist();
+void searchPlayerNumGoals();
+void searchPlayerWeight();
+void searchPlayerHeight();
+void searchPlayerDefense();
+char validateName(char * );
+
+/*********************************/
